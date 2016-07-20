71440485fa4b955d5d83791503337e812e92a1cf
= <dev.kram5@gmail.com>
2016-07-20 11:02:56 +0800

    test commit

--- app/src/main/java/com/nkraft/pg/MainActivity.java
+++ app/src/main/java/com/nkraft/pg/MainActivity.java
@@ -3,11 +3,15 @@ package com.nkraft.pg;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.nkraft.pg.utils.Debug;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Debug.log("hey there: %s", "mark");
     }
 }
---
+++ app/src/main/java/com/nkraft/pg/utils/Debug.java
@@ -0,0 +1,10 @@
+package com.nkraft.pg.utils;
+
+/**
+ * Created by mark on 20/07/2016.
+ */
+public class Debug {
+    public static void log(String message, Object ...args) {
+
+    }
+}
