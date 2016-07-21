f7aefc07d695a27c72c47970d7419d904e31232f
= <dev.kram5@gmail.com>
2016-07-21 11:01:00 +0800

    add sample view

---
+++ app/src/main/java/com/nkraft/pogo/view/SampleView.java
@@ -0,0 +1,14 @@
+package com.nkraft.pogo.view;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+
+/**
+ * Created by mark on 21/07/2016.
+ */
+public class SampleView extends View {
+    public SampleView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+}
